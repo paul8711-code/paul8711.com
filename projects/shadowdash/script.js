60d1007478f056c7f3b38b14d05296fd757c727e
Paul8711 <154304377+Paulprojects8711@users.noreply.github.com>
2026-03-23 16:59:12 +0100

    fix bugs

--- projects/shadowdash/script.js
+++ projects/shadowdash/script.js
@@ -1,29 +1,21 @@
-let slideIndex = 1;
+let slideIndex = 0;
 showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function minusSlides(n) {
+    showSlides(slideIndex -= n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("images");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length - 1) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slideIndex++;
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[slideIndex].style.display = "block";
 }
